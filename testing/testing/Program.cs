552da6e614dd50f46622881245e7ed5a45ee9a4f
anusha <anusweetie.me@gmail.com>
2022-08-26 00:35:59 +0800

    first commit

--- testing/testing/Program.cs
+++ testing/testing/Program.cs
@@ -5,6 +5,7 @@ IWebDriver driver = new ChromeDriver();
 driver.Manage().Window.Maximize();
 
 driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+Thread.Sleep(2000);
 
 IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
 usernameTextbox.SendKeys("hari");
@@ -25,4 +26,48 @@ if (helloHari.Text == "Hello hari!")
 else
 {
     Console.WriteLine("login failed,test failed");
+}
+
+IWebElement AdministrationButton = driver.FindElement(By.XPath(" / html / body / div[3] / div / div / ul / li[5] / a"));
+AdministrationButton.Click();
+
+IWebElement TimematerialOption = driver.FindElement(By.XPath("/ html / body / div[3] / div / div / ul / li[5] / ul / li[3] / a"));
+TimematerialOption.Click();
+
+IWebElement CreateNewButton = driver.FindElement(By.XPath("//*[@id=\'container\']/p/a"));
+CreateNewButton.Click();
+
+IWebElement TypeCodeDropDown = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[1]/div/span[1]/span/span[2]/span"));
+TypeCodeDropDown.Click();
+
+IWebElement codeTextBox = driver.FindElement(By.Id("Code"));
+codeTextBox.SendKeys("abc");
+
+IWebElement DescriptionTextBox = driver.FindElement(By.Id("Description"));
+DescriptionTextBox.SendKeys("abc");
+
+IWebElement PriceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+PriceTag.Click();
+
+IWebElement PriceTextBox = driver.FindElement(By.Id("Price"));
+PriceTextBox.SendKeys("22");
+
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+Thread.Sleep(4000);
+
+IWebElement GoToLastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]"));
+GoToLastPageButton.Click();
+
+IWebElement NewCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+if (NewCode.Text == "abc")
+{
+    Console.WriteLine("new record successfully created.");
+ }
+
+else
+
+{
+    Console.WriteLine("new record unsuccessfull.");
+
 }
